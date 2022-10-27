5d3df6c6d00255429756ae47327ee5153c528b3e
wangyuelei <84227396@qq.com>
2022-10-27 16:50:41 +0800

    first commit

--- src/views/main-content.vue
+++ src/views/main-content.vue
@@ -5,25 +5,31 @@
       <el-dropdown class="aui-content--tabs-tools">
         <i class="el-icon-arrow-down"></i>
         <el-dropdown-menu slot="dropdown" :show-timeout="0">
-          <el-dropdown-item @click.native="tabRemoveHandle($store.state.contentTabsActiveName)">{{ $t('contentTabs.closeCurrent') }}</el-dropdown-item>
+          <el-dropdown-item @click.native="tabRemoveHandle($store.state.contentTabsActiveName)">
+            {{ $t('contentTabs.closeCurrent') }}
+          </el-dropdown-item>
           <el-dropdown-item @click.native="tabsCloseOtherHandle()">{{ $t('contentTabs.closeOther') }}</el-dropdown-item>
           <el-dropdown-item @click.native="tabsCloseAllHandle()">{{ $t('contentTabs.closeAll') }}</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
-      <el-tabs v-model="$store.state.contentTabsActiveName" @tab-click="tabSelectedHandle" @tab-remove="tabRemoveHandle">
+      <el-tabs v-model="$store.state.contentTabsActiveName" @tab-click="tabSelectedHandle"
+               @tab-remove="tabRemoveHandle">
         <el-tab-pane
-          v-for="item in $store.state.contentTabs"
-          :key="item.name"
-          :name="item.name"
-          :label="item.title"
-          :closable="item.name !== 'home'"
-          :class="{ 'is-iframe': tabIsIframe(item.iframeURL) }">
+            v-for="item in $store.state.contentTabs"
+            :key="item.name"
+            :name="item.name"
+            :label="item.title"
+            :closable="item.name !== 'home'"
+            :class="{ 'is-iframe': tabIsIframe(item.iframeURL) }">
           <template v-if="item.name === 'home'">
-            <svg slot="label" class="icon-svg aui-content--tabs-icon-nav" aria-hidden="true"><use xlink:href="#icon-home"></use></svg>
+            <svg slot="label" class="icon-svg aui-content--tabs-icon-nav" aria-hidden="true">
+              <use xlink:href="#icon-home"></use>
+            </svg>
           </template>
-          <iframe v-if="tabIsIframe(item.iframeURL)" :src="item.iframeURL" width="100%" height="100%" frameborder="0" scrolling="yes"></iframe>
+          <iframe v-if="tabIsIframe(item.iframeURL)" :src="item.iframeURL" width="100%" height="100%" frameborder="0"
+                  scrolling="yes"></iframe>
           <keep-alive v-else>
-            <router-view v-if="item.name === $store.state.contentTabsActiveName" />
+            <router-view v-if="item.name === $store.state.contentTabsActiveName"/>
           </keep-alive>
         </el-tab-pane>
       </el-tabs>
@@ -31,37 +37,37 @@
     <!-- 其他方式, 展示内容 -->
     <template v-else>
       <keep-alive>
-        <router-view />
+        <router-view/>
       </keep-alive>
     </template>
   </main>
 </template>
 
 <script>
-import { isURL } from '@/utils/validate'
+import {isURL} from '@/utils/validate'
+
 export default {
-  data () {
-    return {
-    }
+  data() {
+    return {}
   },
   methods: {
     // tabs, 是否通过iframe展示
-    tabIsIframe (url) {
+    tabIsIframe(url) {
       return isURL(url)
     },
     // tabs, 选中tab
-    tabSelectedHandle (tab) {
+    tabSelectedHandle(tab) {
       tab = this.$store.state.contentTabs.filter(item => item.name === tab.name)[0]
       if (tab) {
         this.$router.push({
           'name': tab.name,
-          'params': { ...tab.params },
-          'query': { ...tab.query }
+          'params': {...tab.params},
+          'query': {...tab.query}
         })
       }
     },
     // tabs, 删除tab
-    tabRemoveHandle (tabName) {
+    tabRemoveHandle(tabName) {
       if (tabName === 'home') {
         return false
       }
@@ -75,21 +81,21 @@ export default {
         let tab = this.$store.state.contentTabs[this.$store.state.contentTabs.length - 1]
         this.$router.push({
           name: tab.name,
-          params: { ...tab.params },
-          query: { ...tab.query }
+          params: {...tab.params},
+          query: {...tab.query}
         })
       }
     },
     // tabs, 关闭其它
-    tabsCloseOtherHandle () {
+    tabsCloseOtherHandle() {
       this.$store.state.contentTabs = this.$store.state.contentTabs.filter(item => {
         return item.name === 'home' || item.name === this.$store.state.contentTabsActiveName
       })
     },
     // tabs, 关闭全部
-    tabsCloseAllHandle () {
+    tabsCloseAllHandle() {
       this.$store.state.contentTabs = this.$store.state.contentTabs.filter(item => item.name === 'home')
-      this.$router.push({ name: 'home' })
+      this.$router.push({name: 'home'})
     }
   }
 }
